b512592a4bd5deb14b8d5f26f7151d219bbd160d
Tim 'mithro' Ansell <mithro@mithis.com>
2015-05-03 12:06:50 +1000

    Remove file before downloading.

--- get-kicad-netlist-reader.sh
+++ get-kicad-netlist-reader.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+rm kicad_netlist_reader.py
 wget http://bazaar.launchpad.net/~kicad-product-committers/kicad/product/download/head:/ky_generic_netlist_r-20130614143754-1rips8zqtq3lmd65-2/kicad_netlist_reader.py
